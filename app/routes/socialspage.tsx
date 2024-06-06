08db4f4b90a774c0cc3398110381d3c001631f08
Mehul Behera <mbehera@umd.edu>
2024-06-06 18:56:53 -0400

    made carousel full screen

--- app/routes/socialspage.tsx
+++ app/routes/socialspage.tsx
@@ -1,26 +1,17 @@
-import Navbar from './navigation';
-import LinkedinCard from './SocialCards/linkedincard';
-import GithubCard from './SocialCards/githubcard';
+import Navbar from "./navigation";
+import LinkedinCard from "./SocialCards/linkedincard";
+import GithubCard from "./SocialCards/githubcard";
 
-function LinksPage(){
-
-    return(
-        <div>
-            <Navbar/>
-            <div className="flex wrap:wrap  justify-content:center ">
-            
-                <LinkedinCard/>
-                <GithubCard/>
-        
-            </div>
-
-        </div>
-        
-
-
-    );
-
-
+function LinksPage() {
+  return (
+    <div>
+      <Navbar />
+      <div className="flex flex-col  justify-content:center ">
+        <LinkedinCard />
+        <GithubCard />
+      </div>
+    </div>
+  );
 }
 
 export default LinksPage;
