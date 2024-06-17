4db6b675efb3b7792bcd292c143d0e6df21c0c8d
Mehul Behera <mbehera@umd.edu>
2024-06-17 17:29:22 -0400

    projects scramble animation

---
+++ app/routes/ProjectCards/tictactoe.tsx
@@ -0,0 +1,21 @@
+function TicTacToeCard() {
+  return (
+    <a
+      href=""
+      className="  hover:animate-wiggle flex flex-col items-center  rounded-lg shadow-lg md:flex-row md:max-w-xl hover:scale-105"
+    >
+      <img
+        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+        src="/linkedin.png"
+        alt=""
+      />
+      <div className="flex flex-col justify-between p-4 leading-normal">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight">
+          TicTacToe Project
+        </h5>
+      </div>
+    </a>
+  );
+}
+
+export default TicTacToeCard;
--- app/routes/aboutpage.tsx
+++ app/routes/aboutpage.tsx
@@ -1,6 +1,9 @@
 import Navbar from "./navigation";
 import LinkedinCard from "./SocialCards/linkedincard";
 import GithubCard from "./SocialCards/githubcard";
+import SurveyCard from "./ProjectCards/survey";
+import ChatBotCard from "./ProjectCards/chatbot";
+import TicTacToeCard from "./ProjectCards/tictactoe";
 import React, { useState, useEffect } from "react";
 import { useTransition, animated } from "@react-spring/web";
 import shuffle from "lodash.shuffle";
@@ -8,11 +11,52 @@ import shuffle from "lodash.shuffle";
 const cardData = [
   { component: <LinkedinCard />, height: 96 },
   { component: <GithubCard />, height: 96 },
+  { component: <SurveyCard />, height: 96 },
+  { component: <ChatBotCard />, height: 96 },
+  { component: <TicTacToeCard />, height: 96 },
 ];
 
+// export default function About() {
+//   const [rows, setRows] = useState(cardData);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => setRows(shuffle), 3000);
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   let height = 0;
+//   const transitions = useTransition(
+//     rows.map((data) => ({ ...data, y: (height += data.height) - data.height })),
+//     {
+//       key: (item) => item.component.type.name,
+//       from: { height: 0, opacity: 0 },
+//       leave: { height: 0, opacity: 0 },
+//       enter: ({ y, height }) => ({ y, height, opacity: 1 }),
+//       update: ({ y, height }) => ({ y, height }),
+//     }
+//   );
+
+//   return (
+//     <>
+//       <div>
+//         <Navbar />
+//         <div className="animate-fadeinbouncedown grid mb-8 rounded-lg md:mb-12 md:grid-cols-3 md:grid-rows-2 gap-7 p-8">
+//           {transitions((style, item) => (
+//             <animated.div className="" style={{ ...style }}>
+//               {item.component}
+//             </animated.div>
+//           ))}
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
 export default function About() {
+  //use state to contain rows
   const [rows, setRows] = useState(cardData);
 
+  //use effect to trigger rerender every second
   useEffect(() => {
     const interval = setInterval(() => setRows(shuffle), 800);
     return () => clearInterval(interval);
@@ -34,12 +78,14 @@ export default function About() {
     <>
       <div>
         <Navbar />
-        <div className="animate-fadeinbouncedown grid mb-8 rounded-lg md:mb-12 md:grid-cols-1 gap-7 p-8">
-          {transitions((style, item) => (
-            <animated.div className="" style={{ ...style }}>
-              {item.component}
-            </animated.div>
-          ))}
+        <div className="flex flex-col justify-items-center justify-center items-center">
+          <div className="animate-fadeinbouncedown grid mb-8 rounded-lg md:mb-12 md:grid-cols-1 gap-7 p-8">
+            {transitions((style, item) => (
+              <animated.div className="" style={{ ...style }}>
+                {item.component}
+              </animated.div>
+            ))}
+          </div>
         </div>
       </div>
     </>
