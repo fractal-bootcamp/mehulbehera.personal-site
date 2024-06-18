3a0fafff82922349e3f6e3e0e2c6ac40e5182473
Mehul Behera <mbehera@umd.edu>
2024-06-18 10:29:04 -0400

    added link to deployed tictactoe

--- app/routes/ProjectCards/tictactoe.tsx
+++ app/routes/ProjectCards/tictactoe.tsx
@@ -1,6 +1,11 @@
 function TicTacToeCard() {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <a
+      href="https://mehulbehera-tic-tac-toe-single-player-r93uev8xs.vercel.app/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="card w-96 bg-base-100 shadow-xl"
+    >
       <figure>
         <img src="https://media.istockphoto.com/id/1365567894/vector/hand-drawn-vector-tic-tac-toe-game-noughts-and-crosses-doodle-sketch.jpg?s=612x612&w=0&k=20&c=pSs72urXBp6V8pnXvuJIfX3krtUoFhHaX6fG2g1PxUQ=" />
       </figure>
@@ -11,11 +16,8 @@ function TicTacToeCard() {
           expressJS. You can against other users or against an AI made using the
           minimax algorithm
         </p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">View</button>
-        </div>
       </div>
-    </div>
+    </a>
   );
 }
 
